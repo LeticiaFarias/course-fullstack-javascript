54bdaeb6cdd670aea75288110a114503faa7a3ba
leticiafarias <leticia_farias@alu.ufc.br>
2021-08-31 13:26:10 -0300

    utilizando recursos do browser

---
+++ JS/browser.js
@@ -0,0 +1,7 @@
+alert("Bem vindo! A seguir, informe seus dados.");
+
+let name = prompt("What's your name?");
+let age = prompt("What's your age?'");
+let confirmData = confirm("Your age is " + age + "?")
+
+alert("Name: " + name + "\nAge: " + age);
